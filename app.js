e77c2bc46601d796cd323d9a62eb2b677bf064c4
Mustafa Kose <muhammedmustafa.kose@ogr.deu.edu.tr>
2021-12-16 13:52:41 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    page_name: 'about',
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
